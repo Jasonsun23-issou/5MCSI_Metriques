9cb815a1ae819e77dd3e75ceb9b164790408d65a
Jasonsun23-issou <72598556+Jasonsun23-issou@users.noreply.github.com>
2024-09-11 09:47:02 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,5 +35,26 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
 
+@app.route('/commits/')
+def show_commits():
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    response = requests.get(url)
+    commits_data = response.json()
+    commits_minutes = []
+
+    for commit in commits_data:
+        commit_date = commit['commit']['author']['date']
+        # Appelle la fonction extract_minutes pour récupérer les minutes
+        minutes = extract_minutes(commit_date)
+        commits_minutes.append(minutes.get_json()['minutes'])  # Convertir en JSON et extraire les minutes
+    
+    return jsonify(commits_minutes=commits_minutes)
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
+
 if __name__ == "__main__":
   app.run(debug=True)
